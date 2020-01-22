57af666b75000136af47fe7a0cc917ac7d5c5c21
Nicholas Dongoske <nickdongoske@gmail.com>
2020-01-22 05:10:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,7 +1,8 @@
-def using_push(array, element)
+def using_push(array, string)
 color = "violet"
 array.push(color)
 end
 
-def using_unshift(array, e)
+def using_unshift(array, string)
+  array.unshift(string)
 end
